96528700466bb7975ad738033d75f494b8c4c6dc
Hadi Hosseini <104890237+SeyedHadiHosseini@users.noreply.github.com>
2024-12-09 20:30:16 +0330

    incme in models and some beatification

--- web/models.py
+++ web/models.py
@@ -7,3 +7,16 @@ class Expense(models.Model):
     date = models.DateField()
     amount = models.BigIntegerField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __unicode__(self):
+        return "{}-{}".format(self.date, self.amount)
+
+
+class Income(models.Model):
+    text = models.CharField(max_length=1000)
+    date = models.DateField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __unicode__(self):
+        return "{}-{}".format(self.date, self.amount)
